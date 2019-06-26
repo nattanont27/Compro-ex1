369ec8610d5e150716b9b278f9e90fac864e3d76
sky indigo blue <52160323+nattanont27@users.noreply.github.com>
2019-06-26 15:56:07 +0700

    initial commit

---
+++ Test1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{	cout<<"student code : 62-060216-1017-0\n";
+	cout<<"student name : Nattanont ekearunpong\n";
+	cout<<"Division : Infomation Techology\n";
+	cout<<"Year : 1\n";
+	cout<<"Room : RB\n";
+	cout<<"Sex : male\n";
+	cout<<"Age :18\n"<<endl;
+
+	cout<<"   #\n";
+	cout<<"  #  #\n";
+	cout<<" #    #\n";
+	cout<<"#      #\n";
+	cout<<"########\n";
+	cout<<"#      #\n";
+}
