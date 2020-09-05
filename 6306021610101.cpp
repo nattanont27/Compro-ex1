a8d369b5a2a3fad7a5195ebda0e679c5e1e13277
sky indigo blue <52160323+nattanont27@users.noreply.github.com>
2020-09-06 01:59:21 +0700

    Add files via upload

--- 6306021610101.cpp
+++ 6306021610101.cpp
@@ -1,27 +1,16 @@
-﻿//Calculate age from year born (BE)
-#include <iostream>
+#include<iostream>
 using namespace std;
-int calAge(int); //Create func
-int yearCount;
-int yearIn;
-int thisYear = 2563;
-int main(){
-    cout << "Welcome to MUTIPLE age calculate program" << endl << endl;
-    cout << "Please determine how many year you want to calculate[BE]: ";  //Year insert count
-        cin >> yearCount;
-    cout << "-----------------------------------------------------------" << endl;
-        //Year Loop
-        for (int i=1; i<= yearCount; i++)
-        {
-            cout << "Insert year " << i << ": ";
-                cin >> yearIn;
-            cout << "Age " << i << ": " << calAge(yearIn) << endl; //this is OUTPUT
-        }
-    cout << endl << "Thank you." << endl;
-    system("pause");
-    return 0;
+void main() {
+	cout << "  1st term of 2020		\\45132114   Wanthanee Prachuabsupakij" << endl;
+	cout << "--------------------------------------------------------------" << endl;
+	cout << "| Subject ID | Subject Name				| Score(100) | Grade |" << endl; 
+	cout << "--------------------------------------------------------------" << endl;
+	cout << "|   4570034  | Introduction to combuter			|     85     |  \"A\"" << endl; // use \ after " to put " in cout command 
+	cout << "|   4580247  | English					|     74     |  \"B\"" << endl;
+	cout << "|   4570034  | Design Thinking				|     92     |  \"A\"" << endl;
+	cout << "--------------------------------------------------------------" << endl;
+	cout << "|    TOTAL   | Subject = 3				|  Average = 3.87" << endl;
+	cout << "=========================================================" << endl << endl;
+	cout << "KMUTNB" << endl;
+	system("pause");
 }
-//This is calAge func.
-int calAge(int yearIn){
-    thisYear = 2563 - yearIn;
-    return thisYear;
